4364057e6a904e49bad72b978b9cdac8bb11f913
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-05-24 23:38:42 +0530

    w

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,22 @@
+import { header } from "@/lib/header";
 import Link from "next/link";
 
 const Header = () => {
+  const layHeader = data => {
+    return (
+      <li
+        className="mx-2"
+        key={data.id}
+      >
+        <Link href={data.to}>{data.name}</Link>
+      </li>
+    );
+  };
+
   return (
-    <header>
-      <nav>
-        <ul className="flex items-end ">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>About</li>
-        </ul>
+    <header className="flex items-end justify-center my-4 sticky">
+      <nav className="border rounded-3xl">
+        <ul className="flex items-end justify-end mx-4 text-lg ">{header.map(data => layHeader(data))}</ul>
       </nav>
     </header>
   );
---
+++ src/lib/header.ts
@@ -0,0 +1,17 @@
+export const header = [
+  {
+    id: 1,
+    name: "Home",
+    to: "/",
+  },
+  {
+    id: 2,
+    name: "About",
+    to: "/About",
+  },
+  {
+    id: 3,
+    name: "Projects",
+    to: "/Projects",
+  },
+];
