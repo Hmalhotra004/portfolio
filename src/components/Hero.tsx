b15e9acc8e78408195974d22c1d947432d0cdb18
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-05-25 01:44:15 +0530

    w

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,13 @@
-import { aboutMe } from "@/lib/content";
 import Image from "next/image";
 
 const Hero = () => {
   return (
     <section className="mt-[6.5rem]">
-      <div className="flex justify-evenly">
+      <div className="flex justify-between">
         <div className="flex flex-col justify-start items-start">
           <h1 className="lg:text-5xl">Hardik Malhotra</h1>
           <h6 className="lg:text-3xl">Full Stack Developer</h6>
-          <p>{aboutMe}</p>
+          {/* <p>{aboutMe}</p> */}
         </div>
         <div className="block">
           <Image
@@ -16,7 +15,7 @@ const Hero = () => {
             alt="Profile Pic"
             width={200}
             height={200}
-            style={{ borderRadius: "5px" }}
+            className="rounded-md select-none"
           />
         </div>
       </div>
