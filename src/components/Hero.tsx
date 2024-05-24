c09244f5ea33f94e90012451bfe1736fd444a559
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-05-25 01:33:22 +0530

    w

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,25 @@
+import { aboutMe } from "@/lib/content";
+import Image from "next/image";
+
 const Hero = () => {
   return (
-    <section>
-      <h1>hello</h1>
+    <section className="mt-[6.5rem]">
+      <div className="flex justify-evenly">
+        <div className="flex flex-col justify-start items-start">
+          <h1 className="lg:text-5xl">Hardik Malhotra</h1>
+          <h6 className="lg:text-3xl">Full Stack Developer</h6>
+          <p>{aboutMe}</p>
+        </div>
+        <div className="block">
+          <Image
+            src="/pfp.jpeg"
+            alt="Profile Pic"
+            width={200}
+            height={200}
+            style={{ borderRadius: "5px" }}
+          />
+        </div>
+      </div>
     </section>
   );
 };
