2675c92f0aac08a5f279bfe5648386ef47e332a5
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-05-24 22:27:04 +0530

    w

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <section className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
-      <h1 className="white">hello</h1>
+    <section className="over" >
+      <section className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+        <h1 className="white">hello</h1>
+      </section>
     </section>
   );
 }
