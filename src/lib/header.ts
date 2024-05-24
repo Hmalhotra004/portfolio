4012659cfd38e4873d0ccc00b811d48a10d9ba8d
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-05-25 00:14:26 +0530

    w

--- src/lib/header.ts
+++ src/lib/header.ts
@@ -1,17 +1,27 @@
 export const header = [
   {
     id: 1,
-    name: "Home",
-    to: "/",
-  },
-  {
-    id: 2,
     name: "About",
     to: "/About",
   },
   {
+    id: 2,
+    name: "Skils",
+    to: "/Skils",
+  },
+  {
     id: 3,
+    name: "Experience",
+    to: "/Experience",
+  },
+  {
+    id: 4,
     name: "Projects",
     to: "/Projects",
+  },
+  {
+    id: 5,
+    name: "Education",
+    to: "/Education",
   },
 ];
