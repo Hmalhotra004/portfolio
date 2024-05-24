6d771c4f4c278e7e968f642179ce0f9678d823a1
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-05-24 22:34:32 +0530

    w

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 export default function Home() {
   return (
-    <section className="over" >
-      <section className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
-        <h1 className="white">hello</h1>
-      </section>
+    <section className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      <h1>hello</h1>
     </section>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
     <header>
       <nav>
-        <ul>
-          <li>Home</li>
+        <ul className="flex items-end ">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>About</li>
         </ul>
       </nav>
     </header>
