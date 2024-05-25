374f0e731c5b5c159a93a4bf7767502c35fd4d52
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-05-25 22:59:26 +0530

    w

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,11 +4,11 @@ import ImageCard from "./ImageCard";
 const Hero = () => {
   return (
     <section className="mt-[4.5rem]">
-      <div className="grid grid-cols-2 justify-evenly gap-4">
+      <div className="grid grid-cols-2 justify-around gap-4">
         <div className="flex justify-center items-center">
           {/* <h1 className="lg:text-5xl">Hardik Malhotra</h1>
           <h6 className="lg:text-3xl">Full Stack Developer</h6> */}
-          <p className="lg:text-xl p-5 xl:p-14 text-left">{aboutMe}</p>
+          <p className=" text-justify lg:text-lg xl:text-1xl md:text-base ">{aboutMe}</p>
         </div>
         <div className="flex justify-center items-center">
           <ImageCard />
