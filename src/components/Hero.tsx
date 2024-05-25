03ce2291ef38a2bfb40f1d9764b35bf8886b3dd9
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-05-25 22:27:32 +0530

    w

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,10 +5,10 @@ const Hero = () => {
   return (
     <section className="mt-[4.5rem]">
       <div className="flex justify-between items-center">
-        <div className="flex flex-col justify-center items-center">
+        <div className="grid grid-cols-2 justify-center items-center">
           {/* <h1 className="lg:text-5xl">Hardik Malhotra</h1>
           <h6 className="lg:text-3xl">Full Stack Developer</h6> */}
-          <p className="text-xl p-5 text-justify">{aboutMe}</p>
+          <p className="lg:text-xl p-5 text-left">{aboutMe}</p>
         </div>
         <div className="block">
           <ImageCard />
--- src/lib/content.ts
+++ src/lib/content.ts
@@ -1,4 +1,4 @@
-export const aboutMe = `I am a passionate Full Stack developer with a knack for crafting robust and scalable web applications. 
+export const aboutMe = `I am a passionate Full Stack Developer with a knack for crafting robust and scalable web applications. 
 I have Honed my Skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js 
 Express.js, MySQL, PostgreSQL. My goal is to leverage my expertise to create innovative solutions that drive business 
 growth and deliver exceptional user experiences.`;
