e7be5d0a2458494dd62dc01fa837bfbe7cc3c249
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-05-31 14:15:22 +0530

    w

--- src/components/Other/ProjectCard.tsx
+++ src/components/Other/ProjectCard.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const ProjectCard = ({ project }: { project: any }) => {
   return (
-    <div className="rounded-2xl border-4 border-neutral-800 p-4">
+    <div className="rounded-2xl border-4 border-neutral-800 p-4 max-w-[17rem] max-h-[25rem]">
       <div className="w-full flex justify-center">
         <Image
           src={project.image}
@@ -12,7 +12,7 @@ const ProjectCard = ({ project }: { project: any }) => {
           className="mb-4 rounded"
         />
       </div>
-      <div className="w-fit max-w-fit">
+      <div className="w-fit max-w-fit border border-white">
         <h6 className="mb-2 font-semibold">{project.title}</h6>
         <p className="mb-4 text-neutral-400">{project.des}</p>
         <div className="flex flex-wrap">
