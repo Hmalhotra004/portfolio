dbfa6612ff0045783b7e8e96639f4accb16e42e0
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-05-31 16:38:50 +0530

    w

--- src/components/Other/NavBarLogos.tsx
+++ src/components/Other/NavBarLogos.tsx
@@ -1,3 +1,5 @@
+import { FadeIn } from "@/lib/animation";
+import { motion } from "framer-motion";
 import { FaInstagram, FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa6";
 
@@ -5,24 +7,33 @@ const NavBarLogos = () => {
   const logoStyle = "mx-2 md:text-2xl xxs:text-xl hover:text-purple-500 transition-all ";
   return (
     <nav className="flex justify-center items-center">
-      <a
+      <motion.a
+        variants={FadeIn(0)}
+        initial="hidden"
+        animate="visible"
         href="https://www.linkedin.com/in/hardik-malhotra-726328299/"
         target="_blank"
       >
         <FaLinkedin className={logoStyle} />
-      </a>
-      <a
+      </motion.a>
+      <motion.a
+        variants={FadeIn(0)}
+        initial="hidden"
+        animate="visible"
         href="https://github.com/Hmalhotra004"
         target="_blank"
       >
         <FaGithub className={logoStyle} />
-      </a>
-      <a
+      </motion.a>
+      <motion.a
+        variants={FadeIn(0)}
+        initial="hidden"
+        animate="visible"
         href="https://www.instagram.com/hardik152004/"
         target="_blank"
       >
         <FaInstagram className={logoStyle} />
-      </a>
+      </motion.a>
     </nav>
   );
 };
