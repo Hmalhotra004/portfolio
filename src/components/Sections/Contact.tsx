d8ba449f5ff16fb2ec55801bbdca9011171d1b92
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-06-07 15:28:36 +0530

    w

--- src/components/Sections/Contact.tsx
+++ src/components/Sections/Contact.tsx
@@ -13,6 +13,26 @@ const Contact = () => {
       >
         <h2 className="my-16 text-center text-4xl">Get in Touch</h2>
       </Reveal>
+      <Reveal
+        x={-400}
+        cn="flex mx-auto"
+      >
+        <p className="text-xl mb-4">+91 7715871202</p>
+      </Reveal>
+
+      <Reveal
+        x={400}
+        cn="flex mx-auto"
+      >
+        <a
+          href="#"
+          className="text-xl"
+        >
+          hardikmalhotra150804@gmail.com
+        </a>
+      </Reveal>
+
+      <button className="text-purple-500 bg-neutral-900 text-2xl p-3 rounded-lg">Resume</button>
     </section>
   );
 };
