2f1910d81248f3a25d3bfd97170f77f6213cf9f9
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-06-10 14:11:46 +0530

    w

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,19 +1,19 @@
 export const Projects = [
   {
-    title: `Tic-Tac-Toe`,
-    image: "/tic.png",
-    des: `A Tic-Tac-Toe Game`,
-    link: "https://tic-tac-toe-three-peach.vercel.app",
-    github: "https://github.com/HardikMalhotra/Tic-Tac-Toe",
-    tech: ["HTML", "CSS", "ES6"],
-  },
-  {
     title: `NMTechFest'24 Website`,
     image: "/tic.png",
     des: `Official website of NMTechfest'24`,
     link: "https://techfest-website-24.vercel.app",
     github: "https://github.com/Hmalhotra004/techfest-website-24",
     tech: ["Next.js", "React.js", "ES6", "SCSS", "Framer Motion"],
+  },
+  {
+    title: `Tic-Tac-Toe`,
+    image: "/tic.png",
+    des: `A Tic-Tac-Toe Game`,
+    link: "https://tic-tac-toe-three-peach.vercel.app",
+    github: "https://github.com/HardikMalhotra/Tic-Tac-Toe",
+    tech: ["HTML", "CSS", "ES6"],
   },
   // {
   //   title: `The Simon Game`,
