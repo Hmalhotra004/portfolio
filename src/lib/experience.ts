31c50e1824a0340abb1ba57653e0102c0e571361
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-08-13 19:30:58 +0530

    updated experience

--- src/lib/experience.ts
+++ src/lib/experience.ts
@@ -1,14 +1,14 @@
 export const EXPERIENCE = [
   {
-    year: "May 2024 - Current",
+    year: "May 2024 - August 2024",
     role: "Web Developer Intern",
     company: "ChakliArt",
-    description: `Creating the official company website`,
+    description: `Design and develop websites for Chakliart and its clients, ensuring responsive and user-friendly interfaces.`,
     skiils: ["Next.js", "Typescript", "SCSS", "Framer Motion"],
   },
 
   {
-    year: "Aug 2023 - March 2024",
+    year: "August 2023 - March 2024",
     role: "Head of Department Technicals",
     company: "NMTechfest'24",
     description: `Leading the Technicals Team that is responsible
